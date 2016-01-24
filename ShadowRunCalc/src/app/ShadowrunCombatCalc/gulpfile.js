e98a4bb0978e6b0e546328a6f5da5de4eb0a1fe4
cfriedel <chris.friedel@gmail.com>
2016-01-23 18:27:22 -0700

    1.0.5 Moved to SCSS Basic config for gulp

--- ShadowRunCalc/src/app/ShadowrunCombatCalc/gulpfile.js
+++ ShadowRunCalc/src/app/ShadowrunCombatCalc/gulpfile.js
@@ -17,14 +17,15 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    //.pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./www/css/'))    
     .on('end', done);
 });
+
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
